59001b756c2f437bdc463c4a4c57699e4391cfd3
Dillon O'Leary <do-leary@royal-03.cs.wisc.edu>
2018-10-31 14:06:30 -0500

    Is the hashing working correctly?

--- hash_map.c
+++ hash_map.c
@@ -3,13 +3,33 @@
 #include <stdlib.h>
 
 void createMap(DAG_map * d_map, int size, BuildSpecList * specs) {
-    if (NULL == (d_map->map = malloc(sizeof(BuildSpecNode*) * (int)((double)size / LOAD_FACTOR)))) {
+    int arr_size = (int)(size / LOAD_FACTOR);
+    if (NULL == (d_map->map = malloc(sizeof(BuildSpecNode*) * arr_size))) {
         fprintf(stderr, "Memory allocation error");
         exit(-1);
     }
-    d_map->size = size;
+    //d_map->size = size;
+    d_map->size = arr_size;
     // FIXME assert the first element is the root
     d_map->root = specs->list[0]->target;
+}
+
+// FIXME can I use this code?? its from http://www.cse.yorku.ca/~oz/hash.html
+unsigned long
+hash(unsigned char *str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
+
+int getIndex(char * str, int size) {
+    // FIXME is this the correct casting?
+    return hash( (unsigned char*) str) % size;
 }
 
 void populateMap(DAG_map * map, BuildSpecList* specs) {
@@ -18,7 +38,8 @@ void populateMap(DAG_map * map, BuildSpecList* specs) {
         node->data = specs->list[i];
         node->tempMark = node->permMark = 0;
         node->children = specs->list[i]->deps;
-        map->map[i] = node;
+        //map->map[i] = node;
+        map->map[getIndex(specs->list[i]->target, map->size)] = node;
     }
 }
 
--- test_hash.c
+++ test_hash.c
@@ -13,11 +13,11 @@ BuildSpec * createSpec(Command * cmd, char * target, char ** deps, int depsLen) 
 }
 
 void printMap(DAG_map * map) {
-    printf("reaches printmap\n");
-    int i;
-    for (i = 0; i < 5; i++) {
+    for (int i = 0; i < map->size; i++) {
         printf("loop %d\n", i );
-        printf("Element at %d: %s, with dependeny 0: %s, and cmd: %s\n", i, "NI", "NI","NI");//map->map[i]->data->target, "NI","NI"); 
+        if (map->map[i] == NULL)
+            continue;
+        printf("Element at %d: %s, with dependeny 0: %s, and cmd: %s\n", i, map->map[i]->data->target, "NI","NI"); 
     }
 }
 
@@ -31,12 +31,11 @@ int main() {
     specs.len = 5;
     specs.list = malloc(sizeof(BuildSpec*)*5);
     specs.list[0] = createSpec(cmd, "TEST0", deps, depsLen);
-    printf("TES: %s\n",specs.list[0]->target);
-    specs.list[1] = createSpec(cmd, "TEST1", deps, depsLen);
+    specs.list[1] = createSpec(cmd, "TESTe1", deps, depsLen);
     specs.list[2] = createSpec(cmd, "TEST2", deps, depsLen);
     specs.list[3] = createSpec(cmd, "TEST3", deps, depsLen);
     specs.list[4] = createSpec(cmd, "TEST4", deps, depsLen);
-    createMap(map, depsLen, &specs); 
+    createMap(map, specs.len, &specs); 
     populateMap(map, &specs);
     printMap(map);
 }
