cdf9951f43ae6aac6966daec88f752b1f1d265bb
Dillon O'Leary <do-leary@rockhopper-03.cs.wisc.edu>
2018-11-01 15:35:47 -0500

    Need to fix filename and I think that the commands aren't coming out correctly

--- assess_specs.c
+++ assess_specs.c
@@ -48,6 +48,12 @@ time_t getLastMod(char* filename) {
  * to be recompiled, as in it doesn't exist
  */
 int targetOlderThanDeps(char* target, char** deps, int depsLen) {
+    // FIXME this isn't right!! it can have no dependencies and not be a file! i can have
+    // no commands and still not be a file! I need some sort of flag...
+    if (depsLen == 0) {
+        printf("%s is a file!!\n", target);
+        return 1;
+    }
     for (int i=0; i<depsLen; i++) {
         if (getLastMod(target) > getLastMod(deps[i]))
             return 1;
