829378ec14b6065249989903e19ceb7ab0c80d61
Dillon O'Leary <do-leary@royal-30.cs.wisc.edu>
2018-10-31 19:30:49 -0500

    should be able to detect cycles

--- assess_specs.c
+++ assess_specs.c
@@ -1,13 +1,29 @@
+#include <stdlib.h>
 #include "assess_specs.h"
 #include "hash_map.h"
 
-void visitNode(BuildSpecNode * node, CommandList * cmdList) {
+// TODO I need a way to add to the command list without knowing
+// how many times, but I know I wont remove
+void visitNode(DAG_map * map, BuildSpecNode * node, CommandList * cmdList) {
+    printf("Visit node %s\n", node->data->target);
 //    if n has a permanent mark then return
+    if (node->permMark == 1) {
+        return;
+    }
 //    if n has a temporary mark then stop   (not a DAG)
+    if (node->tempMark == 1) {
+        fprintf(stderr, "Error: This makefile contains a cycle!\n");
+        exit(-1);
+    }
 //    mark n temporarily
+    node->tempMark = 1;
 //    for each node m with an edge from n to m do
 //        visit(m)
+    for (int i=0; i<node->data->depsLen; i++) {
+        visitNode(map, lookup(map, node->data->deps[i]), cmdList);
+    }
 //    mark n permanently
+    node->permMark = 1;
 //    add n to head of L
 }
 
@@ -15,7 +31,7 @@ int getCommandList(CommandList * cmdList, BuildSpecList * list) {
     DAG_map map;
     initHashMap(&map, list);
     // create a list of commands
-    visitNode(lookup(&map, map.root), cmdList);
+    visitNode(&map, lookup(&map, map.root), cmdList);
     return 0;
 }
 
--- test_create_specs.c
+++ test_create_specs.c
@@ -15,6 +15,30 @@ BuildSpec * createSpec(Command * cmd, char * target, char ** deps, int depsLen) 
 
 void createSpecList(BuildSpecList * specs) {
     Command * cmd = NULL;
+    char** deps;
+    specs->len = 4;
+    specs->list = malloc(sizeof(BuildSpec*)*specs->len);
+
+    int depsLen = 2;
+    deps = malloc(sizeof(char*)*depsLen);
+    deps[0] = "b";
+    deps[1] = "c";
+    specs->list[0] = createSpec(cmd, "a", deps, depsLen);
+
+    depsLen = 1;
+    deps = malloc(sizeof(char*)*depsLen);
+    deps[0] = "d";
+    specs->list[1] = createSpec(cmd, "b", deps, depsLen);
+
+    depsLen = 1;
+    deps = malloc(sizeof(char*)*depsLen);
+    deps[0] = "b";
+    specs->list[2] = createSpec(cmd, "c", deps, depsLen);
+
+    depsLen = 0;
+    deps = malloc(sizeof(char*)*depsLen);
+    specs->list[3] = createSpec(cmd, "d", deps, depsLen);
+    /*
     char** deps = NULL;
     int depsLen = 0;
     specs->len = 9;
@@ -28,5 +52,5 @@ void createSpecList(BuildSpecList * specs) {
     specs->list[6] = createSpec(cmd, "TESTy6", deps, depsLen);
     specs->list[7] = createSpec(cmd, "TESTq7", deps, depsLen);
     specs->list[8] = createSpec(cmd, "TESTp8", deps, depsLen);
-
+*/
 }
