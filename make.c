5157bc78f3668084ea78b239701afb11dab819ee
Dillon O'Leary <do-leary@rockhopper-03.cs.wisc.edu>
2018-11-05 21:05:01 -0600

    Started working on main

---
+++ make.c
@@ -0,0 +1,18 @@
+/**
+ * This is the main file for the
+ * make program. It handles the high level function
+ * calls
+ */
+#include "assess_specs.h"
+#include "parser.h"
+#include "parse_text.h"
+
+int main() {
+    FILE * fp;
+    BuildSpecList specs;
+    CommandList cmdList;
+    // TODO read in the args
+    fp = open_makefile(0, "makefile");
+    getBuildSpecList(&specs, fp);
+    getCommandList(&cmdList, &specs);
+}
