12029a996a7a4e1ef6e77cc0ebfef49f6c58aeb9
Ezra Boley <eboley@wisc.edu>
2018-11-05 23:03:19 -0600

    function name change

--- list_utils.h
+++ list_utils.h
@@ -6,7 +6,7 @@ void append_build_spec(BuildSpecList *bsl, BuildSpec *bs);
 
 BuildSpec *get_last_build_spec(BuildSpecList *bsl);
 
-void append_cmd(BuildSpec *bs, Command *cmd);
+void append_cmd_to_buildspec(BuildSpec *bs, Command *cmd);
 
 Command *get_last_cmd(BuildSpec *bs);
 
