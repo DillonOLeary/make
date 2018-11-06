d0d3d0c952ffdc34247e6c5eab96dc1b382d8d08
Dillon O'Leary <doleary@wisc.edu>
2018-11-05 23:00:28 -0600

    Header guards

--- list_utils.h
+++ list_utils.h
@@ -1,11 +1,19 @@
+#ifndef __LIST__UTILS_H__
+#define __LIST__UTILS_H__
 #include "parser.h"
 
 
-
+/**
+ * Append to the build spec list
+ */
 void append_build_spec(BuildSpecList *bsl, BuildSpec *bs);
 
 BuildSpec *get_last_build_spec(BuildSpecList *bsl);
 
+/*
+ * Append to the command list WITHIN the
+ * build spec
+ */
 void append_cmd(BuildSpec *bs, Command *cmd);
 
 Command *get_last_cmd(BuildSpec *bs);
@@ -17,3 +25,5 @@ BuildSpec *next_BS (BuildSpec *bs);
 void append_cmd_to_cmdlist(CommandList *cmdlist, Command *cmd);
 
 void init_cmd_list(CommandList *cmdList);
+
+#endif
